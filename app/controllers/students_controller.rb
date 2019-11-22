8a1827e80a0d4aef8ca8d441921979b1c9eb1293
Joseh <joseph161216@gmail.com>
2019-11-22 13:55:38 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,11 +7,21 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-
+  
   def new
+    @student = Student.new
   end
-
+  
   def create
+    # Failure/Error: @student = Student.create(params)
+    
+    #  ActiveModel::ForbiddenAttributesError:
+    #    ActiveModel::ForbiddenAttributesError
+    # @student = Student.create(params)
+    
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # byebug
+    redirect_to student_path(@student)
   end
 
 end
